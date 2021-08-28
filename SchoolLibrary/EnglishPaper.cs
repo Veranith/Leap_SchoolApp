a5550f2abce87cc32d084225448e6a4ec5f24cb2
Ryan Robinson <274507+Veranith@users.noreply.github.com>
2021-08-27 19:34:31 -0600

    Adds string extension method for word count

--- SchoolLibrary/EnglishPaper.cs
+++ SchoolLibrary/EnglishPaper.cs
@@ -11,5 +11,9 @@ namespace SchoolLibrary
         public string Title { get; set; }
         public int MinimumWordCoubt { get; set; }
         public string PaperText { get; set; }
+        public int WordCount
+        {
+            get { return PaperText.WordCount(); }
+        }
     }
 }
